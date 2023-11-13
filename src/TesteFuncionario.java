9e5ef25fb2b4f12442bcb5126dfd0d715bd3eee5
Robson-Teixeira <robsonmteixeira@outlook.com>
2023-11-13 01:27:16 -0300

    Introdução a herança: Revisão e a classe Funcionário

---
+++ src/TesteFuncionario.java
@@ -0,0 +1,16 @@
+
+public class TesteFuncionario {
+
+	public static void main(String[] args) {
+
+		Funcionario funcionario = new Funcionario();
+		funcionario.setNome("Robson Teixeira");
+		funcionario.setCpf("079.371.220-30");
+		funcionario.setSalario(2590.00);
+
+		System.out.println("Funcionário - Nome: " + funcionario.getNome());
+		System.out.println("Funcionário - Bonificação: " + funcionario.getBonificacao());
+
+	}
+
+}
